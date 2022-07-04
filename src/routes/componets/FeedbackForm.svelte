14673a37ab15a4f584f10990b1a35b017921689f
Ashish <aashish.bhujel@gmail.com>
2022-07-04 21:57:17 +0545

    Added : Rating input and added in form componets

--- src/routes/componets/FeedbackForm.svelte
+++ src/routes/componets/FeedbackForm.svelte
@@ -1,8 +1,11 @@
 <script>
+    import {v4 as uuidv4} from 'uuid'
     import Card from "./Card.svelte";
     import Button from "./Button.svelte"
+    import RatingSelect from "./RatingSelect.svelte"
     
     let text =''
+    let rating = 5
     let btnDisabled =true
     let min  = 10 
     /**
@@ -19,15 +22,30 @@
             btnDisabled = false
         }
     }
-   
+
+    const handleSelect = (/** @type {{ detail: number; }} */ e)=>{
+         rating = e.detail
+    }
+
+    const handleSubmit =()=>{
+        if(text.trim().length >min){
+            const newFeedback = {
+                id: uuidv4(),
+                text,
+                rating : + rating
+            }
+            console.log(newFeedback)
+        }
+        
+    }
 </script>
 
 <Card>
     <header>
         <h2>How would you rate your service with us ?</h2>
     </header>
-    <form>
-        <!-- Rating Select  -->
+    <form on:submit|preventDefault={handleSubmit}>
+       <RatingSelect on:rating-select={handleSelect}/>
         <div class="input-group">
             <input type="text"  on:input={handleInput} bind:value ={text} placeholder="Tell us something that keeps you coming back">
             <!-- svelte-ignore missing-declaration -->
@@ -46,6 +64,22 @@
 
 
 <style>
-   
+   .input-group {
+    display: flex;
+    flex-direction: row;
+    border: 1px solid #ccc;
+    padding: 8px 10px;
+    border-radius: 8px;
+    margin-top: 15px;
+  }
+  input {
+    flex-grow: 2;
+    border: none;
+    font-size: 16px;
+  }
+  input:focus {
+    outline: none;
+  }
+
 
 </style>
---
+++ src/routes/componets/RatingSelect.svelte
@@ -0,0 +1,89 @@
+<script>
+    import {createEventDispatcher} from 'svelte'
+    // your script goes here
+    let selected = 5 
+    
+     const dispatch  =createEventDispatcher()
+
+    const onChange =(/** @type {{ currentTarget: { value: any; }; }} */ e)=>{
+       selected = (e.currentTarget.value)
+       dispatch('rating-select', selected)
+    }
+</script>
+
+<ul class="rating">
+    <li>
+        <input type="radio" id="num1" name="rating" value="1" on:change={onChange} checked={selected===1}
+        />
+        <label for="num1">1</label>    
+    </li>
+    <li>
+        <input type="radio" id="num2" name="rating" value="2" on:change={onChange} checked={selected===2}
+        />
+        <label for="num2">2</label>    
+    </li>
+    <li>
+        <input type="radio" id="num3" name="rating" value="3" on:change={onChange} checked={selected===3}
+        />
+        <label for="num2">3</label>    
+    </li>
+    <li>
+        <input type="radio" id="num4" name="rating" value="4" on:change={onChange} checked={selected===4}
+        />
+        <label for="num4">4</label>    
+    </li>
+    <li>
+        <input type="radio" id="num5" name="rating" value="5" on:change={onChange} checked={selected===5}
+        />
+        <label for="num5">5</label>    
+    </li>
+
+
+</ul>
+
+<style>
+    .rating {
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        margin: 30px 0;
+
+        
+    }
+    .rating li {
+        position: relative;
+        list-style-type: none;
+        background: #f4f4f4;
+        width: 20px;
+        height: 20px;
+       
+        text-align: center;
+        border-radius: 50%;
+        font-size: 20px;
+        border: 1px #eee solid;
+        transition: 0.3s;
+    }
+
+    .rating li label {
+        position: absolute;
+        margin-top: -12px;
+        left: 50%;
+        width:25px;
+        height: 25px;
+   
+        border-radius: 50%;
+        transform: translate(-50%,-50%);
+        cursor: pointer;
+    }
+    .rating li:hover {
+        background: #ff4000c6 ;
+        color: #fff;
+    }
+    [type="radio"]{
+        opacity: 0;
+    }
+    [type="radio"]:checked ~ label {
+        background: #ff3e00 ;
+        color: #fff;
+    }
+</style>
